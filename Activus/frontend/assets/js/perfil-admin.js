fff159862bc10d7076836042f987fe4719283a11
vdldaniel <daniel_vdl090@live.com.ar>
2025-10-04 12:18:53 -0300

    archivo editar-perfil-admin.html

--- Activus/frontend/assets/js/perfil-admin.js
+++ Activus/frontend/assets/js/perfil-admin.js
@@ -1,4 +1,9 @@
-// profile.js
 document.addEventListener("DOMContentLoaded", () => {
   lucide.createIcons();
+
+  const editBtn = document.getElementById("editProfileBtn");
+  editBtn.addEventListener("click", () => {
+    // Redirige a la versión editable del perfil
+    window.location.href = "perfil-admin.html";
+  });
 });
